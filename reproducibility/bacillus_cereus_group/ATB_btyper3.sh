cb9619507f9b30b9065b6bb7e199b04fd04132ae
Vignesh Ramnath <94348700+vigzy77@users.noreply.github.com>
2026-02-18 16:25:26 +0100

    Create ATB_btyper3.sh
    Slurm script to run BTyper3 module (BaT_btyper3.sh) in a loop for all the relevant sylph species

---
+++ reproducibility/bacillus_cereus_group/ATB_btyper3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash -l
+#SBATCH -A projectname
+#SBATCH --time=01:00:00
+#SBATCH -N 1 -c 28
+#SBATCH -J ATB_btyper3
+
+## Load modules
+ml Nextflow/25.10.0
+
+## Arguments
+ATB_maindir=$1 # Parent dirctory for ATB B. cereus group specific analysis
+
+# Run BTyper3 module (executed via Bactopia) in a loop for all the relevant sylph species
+cat ${ATB_maindir}/Bacillus_A_count.tsv | grep -v "^sylph" | cut -f 1 | \
+while read sylph_species; \
+do \
+bash ${ATB_maindir}/src/BaT_btyper3.sh ${sylph_species} ${ATB_maindir}; \
+echo -e "[ATB-btyper3]: Finished BTyper3 analysis for sylph_species - ${sylph_species}"; \
+done
